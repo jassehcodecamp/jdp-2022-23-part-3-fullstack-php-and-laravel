886562aa9ad29aacd3d3662568d45d0b2e288dff
Omar Jasseh <jassehomar99@gmail.com>
2023-10-14 15:29:48 +0000

    week-5 Eloquent Relationships and the Database Query Builder

--- 1-course-outline.md
+++ 1-course-outline.md
@@ -16,12 +16,20 @@
    4. Route Parameters
    5. Route Names
    
-4. Database and Eloquent
+4. Introduction to Databases in Laravel
    1. Database Setup and Configuration
-   2. Introduction to Eloquent
-   3. Migrations
-   4. Query Builder
+   2. Migrations
+   3. Introduction to Eloquent
+   <!-- 4. Query Builder -->
    
-5. Laravel Frontend
-   1. Blade Templating
-   2. Inertia with React and Tailwind CSS
+5. Basic Laravel Frontend
+   1. Intro to Blade Templating
+      1. Displaying Data
+      2. Blade Directives
+      3. Blade Components
+      4. Working with Layouts
+         1. Layouts with Components
+         2. Layouts with Template Inheritance
+   2. Laravel Breeze with Blade
+
+6. Eloquent Relationships & Database Query Builder
---
+++ week-5-eloquent-relationships-and-the-database-query-builder/1-intro-to-eloquent-relationships/app/Http/Controllers/AuthorsController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $authors = Author::with(['user', 'books'])->get();
+
+        return view('authors.index', [
+            'authors' => $authors
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Author $author)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Author $author)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Author $author)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Author $author)
+    {
+        //
+    }
+}
